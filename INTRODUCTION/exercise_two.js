a6193d38d1b4f28fe423a973106c072fdca0a8d8
jideSamuels <sammydammywonder@gmail.com>
2023-06-19 09:41:00 +0100

    new commit

---
+++ INTRODUCTION/exercise_two.js
@@ -0,0 +1,15 @@
+let x = 2;
+let y = 10;
+
+console.log("EXERCISE 1")
+let sum1;
+let result_gotten; 
+
+sum1 = x + y;
+result_gotten = 2 * sum1;
+
+console.log(result_gotten);
+
+console.log("EXERCISE 2")
+let value1 = 20;
+let 
