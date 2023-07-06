34721e06aace32238672e2f1ca77660cb69c3362
jideSamuels <sammydammywonder@gmail.com>
2023-07-06 11:55:06 +0100

    Javascript Arrays

---
+++ INTRODUCTION/arrays.js
@@ -0,0 +1,14 @@
+var arr = new Array(5, 4, "5");
+var arr1 = (1, 2, 3);
+
+arr.push("hello");
+console.log(arr);
+
+arr.pop();
+arr.shift();
+console.log();
+arr.unshift("Hiii");
+var arr2 = arr.concat(arr1);
+
+console.log(arr2);
+
